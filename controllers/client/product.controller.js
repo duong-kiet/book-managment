c917af602f57800fd99d47b1d2dfdfcc30c1f1e4
Duong Kiet <bintotet20041012@gmail.com>
2024-06-06 21:15:26 +0700

    Cau truc lai folder pug

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,8 @@
 // GET
 module.exports.index  = (req, res) => {
-    res.render("client/pages/products/index.pug")
+    res.render("client/pages/products/index.pug", {
+        pageTitle: "Danh sach san pham"
+    })
 }
 
 // POST
