e9c5e55f46c0faf6f2e408b333e9c843274d0ac1
Duong Kiet <bintotet20041012@gmail.com>
2024-06-23 18:12:09 +0700

    Tao model

---
+++ models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    title: String,
+    description: String,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: Boolean
+});
+
+
+const Product = mongoose.model("Product", productSchema, "products"); // ten connection
+
+model.exports = Product;
