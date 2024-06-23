4b4fc70974f0897f3010d36e8815ede01bcc9c93
Duong Kiet <bintotet20041012@gmail.com>
2024-06-23 17:54:24 +0700

    Cau hinh database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 
+const database = require("./config/database.js");
+database.connect();
+
 const routeClient = require("./routes/client/index.route.js")
 
 const app = express();
