86b8a29806d20ed229a41176af1bf0f79c968e1b
Duong Kiet <bintotet20041012@gmail.com>
2024-06-27 14:49:22 +0700

    Lam giao dien danh sach san pham

---
+++ controllers/admin/product.controller.js
@@ -0,0 +1,6 @@
+// GET /admin/products/
+module.exports.index = (req, res) => {
+    res.render("admin/pages/products/index.pug", {
+        pageTitle: "Quan ly san pham"
+    });
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const dashboardRoute = require("./dashboard.route.js")
+const productsRoute = require("./product.route.js")
 
 module.exports.index = (app) => {
     app.use("/admin/dashboard", dashboardRoute); 
+    app.use("/admin/products", productsRoute);
 }
