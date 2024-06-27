47da848c84cb58fdea29504a3a205c2f8a1b8bcd
Duong Kiet <bintotet20041012@gmail.com>
2024-06-27 21:39:36 +0700

    Tinh nang loc theo trang thai

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,7 +2,15 @@ const Product = require("../../models/product.model")
 
 // GET /admin/products/
 module.exports.index = async (req, res) => {
-    const products = await Product.find({});
+    const find = {
+        deleted: false
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status // truy vấn 
+    }
+
+    const products = await Product.find(find);
 
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Quan ly san pham",
