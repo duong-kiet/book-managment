296bf1027e1ec66eea317ef78dc1a444539da2ba
Duong Kiet <bintotet20041012@gmail.com>
2024-06-28 10:32:45 +0700

    Tinh nang loc theo trang thai

---
+++ public/admin/js/script.js
@@ -0,0 +1,28 @@
+// Button Status
+const listButtonStatus = document.querySelectorAll("[button-status]"); // trả về một mảng
+
+if(listButtonStatus.length > 0) {
+    let url = new URL(window.location.href); // lấy ra cửa sổ hiện tại 
+
+    // Bắt sự kiện click
+    listButtonStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+            
+            if(status) {
+                url.searchParams.set("status", status); // thêm tính năng query 
+            } else {
+                url.searchParams.delete("status");
+            }
+
+            window.location.href = url.href;
+        });
+    });
+
+    // Thêm class active mặc định
+    const statusCurrent = url.searchParams.get("status") || "";
+    const buttonCurrent = document.querySelector(`[button-status="${statusCurrent}"]`);
+
+    buttonCurrent.classList.add("active");
+}
+// End Button Status
