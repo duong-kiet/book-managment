fd6997ba67f0c9d9881f094f5c6f16202f5ab7c9
Duong Kiet <bintotet20041012@gmail.com>
2024-06-29 15:35:22 +0700

    Thay doi trang thai mot san pham - Gui len bang phuong thuc GET

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);  
+router.get("/change-status/:statusChange/:id", controller.changeStatus);  // : cho thằng động 
 
 module.exports = router;
    
