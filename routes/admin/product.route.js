df8a640432da1bc4c8dbed23e25f224b8f69833b
Duong Kiet <bintotet20041012@gmail.com>
2024-07-01 23:30:42 +0700

    Xoa mem

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const controller = require("../../controllers/admin/product.controller");
 
+// admin/products
 router.get("/", controller.index);  
 
 router.patch("/change-status/:statusChange/:id", controller.changeStatus);  // : cho thằng động
 
 router.patch("/change-multi", controller.changeMulti);
 
-router.delete("/delete/:id", controller.deleteItem);
+router.patch("/delete/:id", controller.deleteItem);
+
+// router.patch("/trash", controller.trash);
 
 module.exports = router;
    
