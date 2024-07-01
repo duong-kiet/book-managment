1fd203c77272a6c59c743061a0ab0ff04f5976f0
Duong Kiet <bintotet20041012@gmail.com>
2024-07-01 15:14:20 +0700

    Thay doi trang thai nhieu san pham - Logic Frontend gui data cho Backend

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,6 +5,7 @@ const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);  
 router.patch("/change-status/:statusChange/:id", controller.changeStatus);  // : cho thằng động 
+router.patch("/change-multi", controller.changeMulti);
 
 module.exports = router;
    
