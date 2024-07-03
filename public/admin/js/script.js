b057f2a46d16423211baedff5daed26ef9e3d3d4
Duong Kiet <bintotet20041012@gmail.com>
2024-07-03 17:03:05 +0700

    Tinh nang tao moi san pham - Preview Anh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -225,3 +225,19 @@ if(showAlert) {
     }, time);
 }
 // End show-alert
+
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+
+if(uploadImage) {
+    const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+    const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+    uploadImageInput.addEventListener("change", () => {
+        const file = uploadImageInput.files[0];
+        if(file) {
+            uploadImagePreview.src = URL.createObjectURL()
+        }
+    })
+}
+// End Upload image
