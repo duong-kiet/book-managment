e440e7b47668e597df668a43d358f0e8270a6a1a
Duong Kiet <bintotet20041012@gmail.com>
2024-07-10 23:57:18 +0700

    Xay dung phan Danh sach nhom quyen

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoute = require("./dashboard.route.js")
 const productsRoute = require("./product.route.js")
 const productsCategoryRoute = require("./product-category.route.js")
+const rolesRoute = require("./role.route.js")
 const systemConfig = require("../../config/system.js")
 
 
@@ -10,4 +11,5 @@ module.exports.index = (app) => {
     app.use(`${path}/dashboard`, dashboardRoute); 
     app.use(`${path}/products`, productsRoute);
     app.use(`${path}/products-category`, productsCategoryRoute);
+    app.use(`${path}/roles`, rolesRoute);
 }
