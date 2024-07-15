5e63974390e7b598fae0910384a952f9e9763e9c
Duong Kiet <bintotet20041012@gmail.com>
2024-07-15 09:28:04 +0700

    Lam trang sua tai khoan

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -49,3 +49,36 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`/${systemConfig.prefixAdmin}/accounts`)
 }
+
+// GET /admin/accounts/edit
+module.exports.edit = async (req, res) => {
+    const id = req.params.id
+    const account = await Account.findOne({
+        _id: id,
+        deleted: false
+    })
+
+    const roles = await Role.find({
+        deleted: false
+    }).select("title")
+
+    res.render("admin/pages/accounts/edit.pug", {
+        pageTitle: "Chỉnh sửa tài khoản admin",
+        roles: roles,
+        account: account
+    });
+}
+
+// PATCH /admin/accounts/edit
+module.exports.editPatch = async (req, res) => {
+   const id = req.params.id
+
+   await Account.updateOne({
+        _id: id,
+        deleted: false
+   }, req.body)
+
+   req.flash("success", "Cập nhập thành công");
+
+   res.redirect("back")
+}
