48c0ec07d11fdc9a2a221c78eacb4279dc263519
Duong Kiet <bintotet20041012@gmail.com>
2024-07-22 16:18:40 +0700

    Them log lich su thay doi san pham (Them moi)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,10 @@
 const Product = require("../../models/product.model")
 const ProductCategory = require("../../models/product-category.model")
+const Account = require("../../models/account.model")
 
 const systemConfig = require("../../config/system");
+const moment = require("moment")
+
 const paginationHelper = require("../../helpers/pagination.helper")
 
 const createTreeHelper = require("../../helpers/createTree.helper")
@@ -56,11 +59,24 @@ module.exports.index = async (req, res) => {
     
     // Hết sắp xếp 
     const products = await Product
-    .find(find)
-    .limit(pagination.limitItems)
-    .skip(pagination.skip)
-    .sort(sort);
+        .find(find)
+        .limit(pagination.limitItems)
+        .skip(pagination.skip)
+        .sort(sort);
 
+    for (const item of products) {
+        if(item.createdBy) {
+            const accountCreated = await Account.findOne({
+                _id: item.createdBy
+            }) 
+    
+            item.createdByFullName = accountCreated.fullName;    
+        } else {
+            item.createdByFullName = ""
+        }
+        
+        item.createdAtFormat = moment(item.createdAt).format("DD/MM/YYYY HH:mm:ss")
+    }
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Quan ly san pham",
         products: products,
@@ -195,6 +211,8 @@ module.exports.createPost = async (req, res) => {
             req.body.position = countProducts + 1;
         }
         
+        req.body.createdBy = res.locals.account.id;
+
         const newProduct = new Product(req.body);
         await newProduct.save();
 
