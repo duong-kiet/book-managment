37f235d7d9c1d231406f8e7ba1c7d3c1f595e3d8
Duong Kiet <bintotet20041012@gmail.com>
2024-07-23 11:57:59 +0700

    Hien thi san pham noi bat va moi nhat

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -14,8 +14,6 @@ module.exports.index  = async (req, res) => {
     for (const item of products) {
         item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
     }
-
-    console.log(products);
     
     res.render("client/pages/products/index.pug", {
         pageTitle: "Danh sach san pham",
