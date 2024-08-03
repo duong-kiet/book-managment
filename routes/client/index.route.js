3d85f771031d10decd2ee7999febf96d56f7bfbd
duong-kiet <bintotet20041012@gmail.com>
2024-08-03 08:29:28 +0700

    Lam giao dien Checkout

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,7 @@ const homeRoute = require("./home.route.js")
 const productRoute = require("./product.route.js")
 const searchRoute = require("./search.route.js")
 const cartRoute = require("./cart.route.js")
+const checkoutRoute = require("./checkout.route.js")
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware.js")
 const cartMiddleware = require("../../middlewares/client/cart.middleware.js")
@@ -19,6 +20,8 @@ module.exports.index = (app) => {
     app.use("/search", searchRoute)
 
     app.use("/cart", cartRoute)
+
+    app.use("/checkout", checkoutRoute)
 }
  // tuong tuong nhu const index ( cho nao const thay bang module.exports)
 
