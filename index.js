5a17b84eda4d8a337c0665ab885253b61fbd85d5
duong-kiet <bintotet20041012@gmail.com>
2024-08-15 16:23:55 +0700

    Làm trang 404

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 routeAdmin.index(app);
 routeClient.index(app); // kieu goi ham index cua routeClient 
 
+
+app.get("*", (req, res) => {
+    res.render("client/pages/errors/404", {
+      pageTitle: "404 Not Found"
+    });
+});
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
